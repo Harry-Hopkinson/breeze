bc89d15cfe0f35306b67a0daedc63ff61da2ed91
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-12-21 22:14:28 +0000

    Fix CheckDupInstall.cpp

--- utils/CheckDupInstall.cpp
+++ utils/CheckDupInstall.cpp
@@ -26,7 +26,7 @@ int checkDupInstall(std::string package) {
     }
   }
   return 0;
-  #ifdef __linux__
+  #elif __linux__
   char *username = std::getenv("USERNAME");
   if (username == nullptr) {
     std::cerr << "Error: Could not get username" << "\n";
@@ -45,14 +45,37 @@ int checkDupInstall(std::string package) {
 
   while (std::getline(file, line)) {
     if (line == package) {
-      Logger::Error("You already have that package installed");
+      std::cerr << "Error: " << package << " already installed" << "\n";
+      return 1;
+
+    }
+  }
+  return 0;
+  #elif __APPLE__
+  char *username = std::getenv("USERNAME");
+  if (username == nullptr) {
+    std::cerr << "Error: Could not get username" << "\n";
+    return 1;
+  }
+  // Construct the path to the file using the username
+  std::string filePath = "/Users/" + std::string(std::getenv("USER")) + "/file-list.txt";
+  std::string line;
+
+  // Open the file
+  std::fstream file(filePath, std::ios::in);
+  if (!file) {
+    std::cerr << "Error: Could not open file" << "\n";
+    return 1;
+  }
+
+  while (std::getline(file, line)) {
+    if (line == package) {
+      std::cerr << "Error: " << package << " already installed" << "\n";
       return 1;
     }
   }
-
-   #endif
-#endif
   return 0;
-};
+  #endif
+}
 
 
